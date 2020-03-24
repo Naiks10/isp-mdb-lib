8c7e168335a5130041412afa8f78d6b10d6f349a
d1slike <merser1q@gmail.com>
2020-03-24 15:38:38 +0300

    fix erl messages

--- stubsV1/erl/erl.go
+++ stubsV1/erl/erl.go
@@ -2,7 +2,13 @@ package erl
 
 import (
 	"encoding/xml"
+	"time"
 )
+
+type StringWithLastModified struct {
+	Value        string    `xml:",chardata"`
+	Lastmodified time.Time `xml:"last_modified,attr"`
+}
 
 type PersonsIncoming struct {
 	XMLName        xml.Name         `xml:"http://erl.msr.com/schemas/master-system/elc persons_incoming"`
@@ -10,24 +16,25 @@ type PersonsIncoming struct {
 }
 
 type PersonIncoming struct {
-	Citizen           Citizen           `xml:"citizen,omitempty"`
-	Inn               string            `xml:"inn,omitempty"`
-	IdentityDocuments IdentityDocuments `xml:"identity_documents,omitempty"`
-	Oms               string            `xml:"oms,omitempty"`
+	Citizen           Citizen                 `xml:"citizen,omitempty"`
+	Inn               *StringWithLastModified `xml:"inn,omitempty"`
+	Oms               *StringWithLastModified `xml:"oms,omitempty"`
+	IdentityDocuments IdentityDocuments       `xml:"identity_documents,omitempty"`
 }
 
 type Citizen struct {
-	CitizenPk string      `xml:"citizen_pk,omitempty"`
-	Name      CitizenName `xml:"name,omitempty"`
-	Birthday  string      `xml:"birthday,omitempty"`
-	Sex       string      `xml:"sex,omitempty"`
-	Snils     string      `xml:"snils,omitempty"`
+	CitizenPk string                  `xml:"citizen_pk,omitempty"`
+	Name      CitizenName             `xml:"name,omitempty"`
+	Birthday  *StringWithLastModified `xml:"birthday,omitempty"`
+	Sex       *StringWithLastModified `xml:"sex,omitempty"`
+	Snils     *StringWithLastModified `xml:"snils,omitempty"`
 }
 
 type CitizenName struct {
-	Surname    string `xml:"surname,omitempty"`
-	Firstname  string `xml:"firstname,omitempty"`
-	Patronymic string `xml:"patronymic,omitempty"`
+	Surname      string    `xml:"surname,omitempty"`
+	Firstname    string    `xml:"firstname,omitempty"`
+	Patronymic   string    `xml:"patronymic,omitempty"`
+	Lastmodified time.Time `xml:"last_modified,attr"`
 }
 
 type IdentityDocuments struct {
@@ -35,11 +42,12 @@ type IdentityDocuments struct {
 }
 
 type Document struct {
-	DoctypePk      string `xml:"doctype_pk,omitempty"`
-	Serial         string `xml:"serial,omitempty"`
-	Number         string `xml:"number,omitempty"`
-	DocIssuedate   string `xml:"doc_issuedate,omitempty"`
-	DocIssuePlace  string `xml:"doc_issue_place,omitempty"`
-	DocauthorityPk string `xml:"docauthority_pk,omitempty"`
-	IsDropped      string `xml:"is_dropped,omitempty"`
+	DoctypePk      string    `xml:"doctype_pk,omitempty"`
+	Serial         string    `xml:"serial,omitempty"`
+	Number         string    `xml:"number,omitempty"`
+	DocIssuedate   string    `xml:"doc_issuedate,omitempty"`
+	DocIssuePlace  string    `xml:"doc_issue_place,omitempty"`
+	DocauthorityPk string    `xml:"docauthority_pk,omitempty"`
+	IsDropped      string    `xml:"is_dropped,omitempty"`
+	Lastmodified   time.Time `xml:"last_modified,attr"`
 }
